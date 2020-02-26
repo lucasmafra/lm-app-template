536aca1f12f24ed55997960ec55842425459463e
Lucas Mafra <lucas.mafra95@gmail.com>
2020-02-26 16:14:36 -0300

    bump to 0.9.0

--- resources/leiningen/new/lm_app/dev.clj
+++ resources/leiningen/new/lm_app/dev.clj
@@ -1,6 +1,15 @@
 (ns dev
   (:require [com.stuartsierra.component :as component]
             [com.stuartsierra.component.repl :refer [set-init start stop reset]]
+            [common-clj.components.config.in-memory-config :as imc]
             [{{sanitized}}.system :refer [system]]))
 
-(set-init (constantly system))
+(def config
+  {:app-name :{{raw-name}}})
+
+(def dev-system
+  (merge system
+         (component/system-map
+          :config (imc/new-config config :dev))))
+
+(set-init (constantly dev-system))
